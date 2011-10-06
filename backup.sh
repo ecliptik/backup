00bf5613c50d42f793353aeb672c289a367a43e7
Micheal Waltz <ecliptik@gmail.com>
2011-10-06 00:40:09 -0700

    Added ability to backup multiple backup directories

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ UUID="8cedcdbd-4ba1-43c1-9fa4-bceb3298a10e"
 DISK="/dev/disk/by-uuid/${UUID}"
 BACKUPCUR=backup.current
 BACKUPOLD=backup.${DATE}
+BACKUPDIRS="/home/ /etc/"
 
 echo "Doing a fsck -ay on ${DISK}" >> ${LOG}
 fsck -ay ${DISK} >> ${LOG} 2>&1
@@ -14,18 +15,20 @@ fsck -ay ${DISK} >> ${LOG} 2>&1
 if mount ${DISK} ${MOUNT} >> ${LOG} 2>&1; then
 echo "Mounted device ${UUID} on ${MOUNT}" >> ${LOG}
 
-   if [ -d ${BACKUPCUR} ]; then
+for DIR in ${BACKUPDIRS}; do
+   if [ -d ${DIR}/${BACKUPCUR} ]; then
 	#Move the old current backup directory to a dated dir
-	mv ${MOUNT}/${BACKUPCUR} ${MOUNT}/${BACKUPOLD}
+	mv ${MOUNT}/${DIR}/${BACKUPCUR} ${MOUNT}/${DIR}/${BACKUPOLD}
 	#Create our new current dir
-	mkdir ${MOUNT}/${BACKUPCUR}
+	mkdir ${MOUNT}/${DIR}/${BACKUPCUR}
 	#Backup using rsync and hard links for incremental backups
-	rsync -av --delete --link-dest=${MOUNT}/${BACKUPOLD} /home/ ${MOUNT}/${BACKUPCUR} >> ${LOG} 2>&1
-	echo "Backup of ${UUID} to ${BACKUPCUR} completed" >> ${LOG}
+	rsync -av --delete --link-dest=${MOUNT}/${DIR}/${BACKUPOLD} ${DIR} ${MOUNT}/${DIR}/${BACKUPCUR} >> ${LOG} 2>&1
+	echo "Backup of ${UUID} to ${MOUNT}/${DIR}/${BACKUPCUR} completed" >> ${LOG}
 	
    else
 	#If there is no current dir assume it wasn't mounted
-	echo "The directory ${MOUNT}/${BACKUPCUR} doesn't exist, failing" >> ${LOG}
+	echo "The directory ${MOUNT}/${DIR}/${BACKUPCUR} doesn't exist, failing" >> ${LOG}
 	exit 1
    fi
+done
 fi
